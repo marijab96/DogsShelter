af6ff6d20800cc8c0bf53a492e01593067ef74f5
Marija Bralić <marija.bralic96@gmail.com>
2021-03-13 21:15:09 +0100

    Add controls for box.

--- src/main/java/controller/BoxProcessor.java
+++ src/main/java/controller/BoxProcessor.java
@@ -3,7 +3,7 @@ package controller;
 
 import model.Box;
 import util.MyException;
-
+import java.math.BigDecimal;
 import java.util.List;
 
 
@@ -23,23 +23,112 @@ public class BoxProcessor extends Processor<Box> {
 
     @Override
     protected void controlCreate() throws MyException {
+
+        controlSize();
+        controlCapacity();
         controlNameSet();
+        controlNameLength();
+        controlIsNameUnique();
+        controlEmployeeSet();
     }
 
     @Override
-    protected void controlUpdate() {
+    protected void controlUpdate() throws MyException {
+
+        controlSize();
+        controlCapacity();
+        controlNameSet();
+        controlNameLength();
+        controlIsNameUnique();
+        controlEmployeeSet();
+
 
     }
 
     @Override
-    protected void controlDelete() {
+    protected void controlDelete() throws MyException{
+
+        controlIsBoxEmpty();
+
 
     }
 
     private void controlNameSet() throws MyException{
+
         if(entity.getName() == null || entity.getName().isEmpty()){
+
             throw new MyException("Name must be set.");
 
         }
     }
+
+    private void controlNameLength() throws MyException{
+
+        if(entity.getName().length() > 20){
+
+            throw new MyException("Name cannot be longer than 20 characters.");
+
+        }
+    }
+
+
+
+    private void controlCapacity() throws MyException{
+
+        if(entity.getCapacity() == null || entity.getCapacity() <= 0){
+
+            throw new MyException("Capacity should be greater than zero.");
+        }
+
+
+    }
+
+    private void controlSize() throws MyException{
+
+        if (entity.getSize() == null || entity.getSize().compareTo(BigDecimal.ZERO) <= 0) {
+
+            throw new MyException("Size should be greater than zero.");
+        }
+
+    }
+
+    private void controlIsBoxEmpty() throws MyException{
+
+        if(entity.getDogs().size() > 0) {
+
+            throw new MyException("Box contains dogs and cannot be deleted.");
+
+        }
+
+    }
+
+    private void controlIsNameUnique() throws MyException{
+
+        var boxList = session
+                .createQuery("from Box b where b.name=:name")
+                .setParameter("name", entity.getName())
+                .list();
+
+        if(boxList.size() > 0){
+
+            throw new MyException("Name of box already exist, please enter other name.");
+
+        }
+
+    }
+
+
+    private void controlEmployeeSet() throws MyException{
+
+        if(entity.getEmployee() == null){
+
+            throw new MyException("Please enter employee for this box.");
+
+        }
+
+    }
+
 }
+
+
+
