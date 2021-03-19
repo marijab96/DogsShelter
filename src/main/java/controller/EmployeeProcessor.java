5123b890387d12a2065b286fd5ea34c18ecbbac4
Marija Bralić <marija.bralic96@gmail.com>
2021-03-19 18:40:39 +0100

    Create control for employee.

--- src/main/java/controller/EmployeeProcessor.java
+++ src/main/java/controller/EmployeeProcessor.java
@@ -2,6 +2,7 @@ package controller;
 
 import model.Employee;
 import util.MyException;
+import util.OibCheck;
 
 import java.util.List;
 
@@ -15,15 +16,84 @@ public class EmployeeProcessor extends Processor<Employee>{
     @Override
     protected void controlCreate() throws MyException {
 
+        controlFirsAndLastNameSet();
+        controlOibCheck();
+        controlMobileNumber();
+
+
     }
 
     @Override
     protected void controlUpdate() throws MyException {
+
+        controlFirsAndLastNameSet();
+        controlOibCheck();
+        controlMobileNumber();
 
     }
 
     @Override
     protected void controlDelete() throws MyException {
 
+        controlIsDogSet();
+        controlIsBoxSet();
     }
+
+    private void controlFirsAndLastNameSet() throws MyException{
+
+        if(entity.getFirstName() == null || entity.getLastName() == null || entity.getFirstName().isEmpty() || entity.getLastName().isEmpty()){
+
+            throw new MyException("First and Last Name must be set.");
+
+        }
+    }
+
+    private void controlOibCheck() throws MyException{
+
+        if(!OibCheck.oibValidation(entity.getOib())){
+
+            throw  new MyException("Oib is not valid.");
+
+        }
+    }
+
+    private void controlMobileNumber() throws MyException{
+
+        if(entity.getMobileNumber() == null || entity.getMobileNumber().isEmpty()){
+
+            throw new MyException("Please set mobile number for this employee.");
+        }
+    }
+
+    private void controlIsDogSet() throws MyException{
+
+        var dogList = session
+                .createQuery("from Dog d where d.employee.id=:id")
+                .setParameter("id", entity.getId())
+                .list();
+
+        if(dogList.size() > 0){
+
+            throw new MyException("That employee is assigned to a dog and can not to be deleted.");
+
+        }
+
+    }
+
+    private void controlIsBoxSet() throws MyException{
+
+        var boxList = session
+                .createQuery("from Box b where b.employee.id=:id")
+                .setParameter("id", entity.getId())
+                .list();
+
+        if(boxList.size() > 0){
+
+            throw new MyException("That employee is assigned to a box and can not to be deleted.");
+
+        }
+
+    }
+
+
 }
