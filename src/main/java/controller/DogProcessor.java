2a16b3f1f36dfd7294603f252308f0f1f0d012b8
Marija Bralić <marija.bralic96@gmail.com>
2021-03-19 17:57:57 +0100

    Create control for dog.

--- src/main/java/controller/DogProcessor.java
+++ src/main/java/controller/DogProcessor.java
@@ -16,15 +16,86 @@ public class DogProcessor extends Processor<Dog>{
     @Override
     protected void controlCreate() throws MyException {
 
+        controlNameSet();
+        controlNameLength();
+        controlChipNumberSet();
+        controlEmployeeSet();
+        controlBoxSet();
+        controlSpeciesSet();
+
     }
 
     @Override
     protected void controlUpdate() throws MyException {
 
+        controlNameSet();
+        controlNameLength();
+        controlChipNumberSet();
+        controlEmployeeSet();
+        controlBoxSet();
+        controlSpeciesSet();
+
     }
 
     @Override
     protected void controlDelete() throws MyException {
+
+    }
+
+
+    private void controlNameSet() throws MyException{
+
+        if(entity.getName() == null || entity.getName().isEmpty()){
+
+            throw new MyException("Name must be set.");
+
+        }
+    }
+
+    private void controlNameLength() throws MyException{
+
+        if(entity.getName().length() > 20){
+
+            throw new MyException("Name cannot be longer than 20 characters.");
+
+        }
+    }
+
+    private void controlChipNumberSet() throws MyException{
+
+        if(entity.getChipNumber() == null || entity.getChipNumber() <= 0 ){
+
+            throw new MyException("Chip number must be set.");
+        }
+    }
+
+    private void controlEmployeeSet() throws MyException{
+
+        if(entity.getEmployee() == null){
+
+            throw new MyException("Please enter employee for this dog.");
+
+        }
+
+    }
+
+    private void controlBoxSet() throws MyException{
+
+        if(entity.getBox() == null){
+
+            throw new MyException("Please enter box for this dog.");
+
+        }
+
+    }
+
+    private void controlSpeciesSet() throws MyException{
+
+        if(entity.getSpecies() == null){
+
+            throw new MyException("Please enter species for this dog.");
+
+        }
 
     }
 }
