16f738ad4b337aab643489af2aaec90882633087
Marija Bralić <marija.bralic96@gmail.com>
2021-03-19 18:51:59 +0100

    Create control for sickness.

--- src/main/java/controller/SicknessProcessor.java
+++ src/main/java/controller/SicknessProcessor.java
@@ -15,15 +15,50 @@ public class SicknessProcessor extends Processor<Sickness>{
     @Override
     protected void controlCreate() throws MyException {
 
+        controlNameSet();
+        controlNameLength();
+        controlDescriptionLengt();
+
     }
 
     @Override
     protected void controlUpdate() throws MyException {
+
+        controlNameSet();
+        controlNameLength();
+        controlDescriptionLengt();
 
     }
 
     @Override
     protected void controlDelete() throws MyException {
 
+    }
+
+
+    private void controlNameSet() throws MyException{
+
+        if(entity.getName() == null || entity.getName().isEmpty()){
+
+            throw new MyException("Name must be set.");
+
+        }
+    }
+
+    private void controlNameLength() throws MyException{
+
+        if(entity.getName().length() > 20){
+
+            throw new MyException("Name cannot be longer than 20 characters.");
+
+        }
+    }
+
+    private void controlDescriptionLengt() throws MyException{
+
+        if(entity.getDescription().length() > 500){
+
+            throw new MyException("Description cannot be longer than 500 characters.");
+        }
     }
 }
