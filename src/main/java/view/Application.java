95c8aad4e6584a9a3ae539de8dfa87e66a26e14a
Marija Bralić <marija.bralic96@gmail.com>
2021-03-21 20:09:30 +0100

    Started creating menu and finished logging in interface.

---
+++ src/main/java/view/Application.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import model.Administrator;
+
+/**
+ *
+ * @author mbralic
+ */
+public class Application {
+    
+    public static final String TITLE_APP= "Dogs Shelter";
+    
+    public static Administrator administrator;
+    
+}
