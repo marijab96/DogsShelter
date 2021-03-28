20b16dc70979bee4257be946c52adc50ec8057f0
Marija Bralić <marija.bralic96@gmail.com>
2021-03-28 19:37:23 +0200

    Started creating menu form.

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -5,6 +5,9 @@
  */
 package view;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
 import model.Box;
 
 
@@ -21,6 +24,27 @@ public class Menu extends javax.swing.JFrame {
     public Menu() {
         initComponents();
         setTitle(Application.TITLE_APP + " " + Application.administrator.getFirstNameLastName());
+        new Time().start();
+    }
+    
+    
+    
+    private class Time extends Thread{
+        
+        private SimpleDateFormat df = 
+                new SimpleDateFormat("dd. MMMM YYYY. HH:mm:ss");
+
+        @Override
+        public void run() {
+            while (true) { 
+                    lblTimeAndDate.setText(df.format(new Date()));
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                }
+            }
+        }
+        
     }
 
     /**
@@ -33,195 +57,133 @@ public class Menu extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        pnlBox = new javax.swing.JPanel();
+        lblTitle = new javax.swing.JLabel();
         lblBox = new javax.swing.JLabel();
-        jPanel3 = new javax.swing.JPanel();
         lblDog = new javax.swing.JLabel();
-        jPanel4 = new javax.swing.JPanel();
         lblEmployee = new javax.swing.JLabel();
-        jPanel5 = new javax.swing.JPanel();
-        lblSpecies = new javax.swing.JLabel();
-        jPanel6 = new javax.swing.JPanel();
         lblSickness = new javax.swing.JLabel();
+        lblSpecies = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
+        lblTimeAndDate = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(137, 196, 244));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jPanel1.setBackground(new java.awt.Color(137, 196, 244));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        pnlBox.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mousePressed(java.awt.event.MouseEvent evt) {
-                pnlBoxMousePressed(evt);
-            }
-        });
+        lblTitle.setFont(new java.awt.Font("Karumbi", 3, 48)); // NOI18N
+        lblTitle.setText("Dogs Shelter");
+        jPanel1.add(lblTitle, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 20, 230, 50));
 
-        lblBox.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
-        lblBox.setText("Box");
+        lblBox.setFont(new java.awt.Font("Purisa", 3, 20)); // NOI18N
+        lblBox.setIcon(new javax.swing.ImageIcon("/home/mbralic/IDEA Projekt/DogsShelter/src/main/resources/iconforbox.png")); // NOI18N
+        lblBox.setText("  Box");
         lblBox.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mousePressed(java.awt.event.MouseEvent evt) {
                 lblBoxMousePressed(evt);
             }
-            public void mouseReleased(java.awt.event.MouseEvent evt) {
-                lblBoxMouseReleased(evt);
+        });
+        jPanel1.add(lblBox, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 120, 170, 80));
+
+        lblDog.setFont(new java.awt.Font("Purisa", 3, 20)); // NOI18N
+        lblDog.setIcon(new javax.swing.ImageIcon("/home/mbralic/IDEA Projekt/DogsShelter/src/main/resources/icondog.png")); // NOI18N
+        lblDog.setText("  Dog");
+        lblDog.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                lblDogMousePressed(evt);
             }
         });
+        jPanel1.add(lblDog, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 200, 150, 70));
 
-        javax.swing.GroupLayout pnlBoxLayout = new javax.swing.GroupLayout(pnlBox);
-        pnlBox.setLayout(pnlBoxLayout);
-        pnlBoxLayout.setHorizontalGroup(
-            pnlBoxLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(pnlBoxLayout.createSequentialGroup()
-                .addGap(28, 28, 28)
-                .addComponent(lblBox, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(43, Short.MAX_VALUE))
-        );
-        pnlBoxLayout.setVerticalGroup(
-            pnlBoxLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlBoxLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(lblBox, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
-        );
+        lblEmployee.setFont(new java.awt.Font("Purisa", 3, 20)); // NOI18N
+        lblEmployee.setIcon(new javax.swing.ImageIcon("/home/mbralic/IDEA Projekt/DogsShelter/src/main/resources/iconemployee.png")); // NOI18N
+        lblEmployee.setText("  Employee");
+        jPanel1.add(lblEmployee, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 290, 210, 60));
 
-        jPanel1.add(pnlBox, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 160, 200, 40));
+        lblSickness.setFont(new java.awt.Font("Purisa", 3, 20)); // NOI18N
+        lblSickness.setIcon(new javax.swing.ImageIcon("/home/mbralic/IDEA Projekt/DogsShelter/src/main/resources/iconforsickness.png")); // NOI18N
+        lblSickness.setText("  Sickness");
+        jPanel1.add(lblSickness, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 450, 230, 60));
 
-        lblDog.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
-        lblDog.setText("Dog");
+        lblSpecies.setFont(new java.awt.Font("Purisa", 3, 20)); // NOI18N
+        lblSpecies.setIcon(new javax.swing.ImageIcon("/home/mbralic/IDEA Projekt/DogsShelter/src/main/resources/iconforspecies.png")); // NOI18N
+        lblSpecies.setText("  Species");
+        jPanel1.add(lblSpecies, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 370, 200, 60));
+
+        jPanel3.setBackground(new java.awt.Color(137, 196, 244));
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
-                .addGap(27, 27, 27)
-                .addComponent(lblDog)
-                .addContainerGap(114, Short.MAX_VALUE))
+            .addGap(0, 210, Short.MAX_VALUE)
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(lblDog)
-                .addContainerGap(31, Short.MAX_VALUE))
+            .addGap(0, 80, Short.MAX_VALUE)
         );
 
-        jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 210, 170, 60));
+        jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 120, 210, 80));
 
-        jPanel4.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 270, 590));
 
-        lblEmployee.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
-        lblEmployee.setText("Employee");
+        jPanel2.setBackground(new java.awt.Color(52, 73, 94));
 
-        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
-        jPanel4.setLayout(jPanel4Layout);
-        jPanel4Layout.setHorizontalGroup(
-            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel4Layout.createSequentialGroup()
-                .addGap(31, 31, 31)
-                .addComponent(lblEmployee)
-                .addContainerGap(61, Short.MAX_VALUE))
-        );
-        jPanel4Layout.setVerticalGroup(
-            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(lblEmployee)
-                .addContainerGap())
-        );
-
-        jPanel1.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 270, 160, 40));
-
-        lblSpecies.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
-        lblSpecies.setText("Species");
-
-        javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
-        jPanel5.setLayout(jPanel5Layout);
-        jPanel5Layout.setHorizontalGroup(
-            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel5Layout.createSequentialGroup()
-                .addGap(28, 28, 28)
-                .addComponent(lblSpecies)
-                .addContainerGap(47, Short.MAX_VALUE))
-        );
-        jPanel5Layout.setVerticalGroup(
-            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel5Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(lblSpecies)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-
-        jPanel1.add(jPanel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 320, 130, 40));
-
-        lblSickness.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
-        lblSickness.setText("Sickness");
-
-        javax.swing.GroupLayout jPanel6Layout = new javax.swing.GroupLayout(jPanel6);
-        jPanel6.setLayout(jPanel6Layout);
-        jPanel6Layout.setHorizontalGroup(
-            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel6Layout.createSequentialGroup()
-                .addGap(24, 24, 24)
-                .addComponent(lblSickness)
-                .addContainerGap(36, Short.MAX_VALUE))
-        );
-        jPanel6Layout.setVerticalGroup(
-            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel6Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(lblSickness)
-                .addContainerGap(41, Short.MAX_VALUE))
-        );
-
-        jPanel1.add(jPanel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 370, -1, 70));
-
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 260, 590));
+        lblTimeAndDate.setFont(new java.awt.Font("Purisa", 3, 12)); // NOI18N
+        lblTimeAndDate.setForeground(new java.awt.Color(153, 255, 255));
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 790, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(567, Short.MAX_VALUE)
+                .addComponent(lblTimeAndDate, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 70, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(lblTimeAndDate, javax.swing.GroupLayout.DEFAULT_SIZE, 38, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 520, 790, 70));
+
+        jLabel4.setIcon(new javax.swing.ImageIcon("/home/mbralic/IDEA Projekt/DogsShelter/src/main/resources/homepage.jpg")); // NOI18N
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 0, 780, 520));
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void pnlBoxMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_pnlBoxMousePressed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_pnlBoxMousePressed
-
     private void lblBoxMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblBoxMousePressed
-       
+        new Boxes().setVisible(true);
     }//GEN-LAST:event_lblBoxMousePressed
 
-    private void lblBoxMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblBoxMouseReleased
-       new Boxes().setVisible(true);
-    }//GEN-LAST:event_lblBoxMouseReleased
+    private void lblDogMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblDogMousePressed
+       new Dogs().setVisible(true);
+    }//GEN-LAST:event_lblDogMousePressed
 
     
     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
-    private javax.swing.JPanel jPanel4;
-    private javax.swing.JPanel jPanel5;
-    private javax.swing.JPanel jPanel6;
     private javax.swing.JLabel lblBox;
     private javax.swing.JLabel lblDog;
     private javax.swing.JLabel lblEmployee;
     private javax.swing.JLabel lblSickness;
     private javax.swing.JLabel lblSpecies;
-    private javax.swing.JPanel pnlBox;
+    private javax.swing.JLabel lblTimeAndDate;
+    private javax.swing.JLabel lblTitle;
     // End of variables declaration//GEN-END:variables
 }
